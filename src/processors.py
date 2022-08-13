45a486dfc59d92c278377a55526d91c3efbc02ef
Aristide <aristide.bamazi@imsp-uac.org>
2022-08-13 15:27:45 +0100

    add code for http and ftp processors

--- src/processors.py
+++ src/processors.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+import math
+from typing import Tuple, Optional
 from functools import wraps, cached_property
 from asyncio import Queue
 import abc
@@ -6,13 +7,17 @@ import os
 import shutil
 import logging
 
+from pydantic import parse_obj_as, error_wrappers
+
 import mode
-from aiofiles.os import wrap
 import aiohttp
 import aioftp
+import aiofiles
+from aiofiles.os import wrap
+from aiohttp_retry import RetryClient, ExponentialRetry
 
 from api.models import LogEntry, ProtocolEnum, StatusEnum
-from .utils import PATH, get_protocol
+from .utils import PATH, get_protocol, FtpUrl
 from .notifiers import Notifier
 
 
@@ -43,6 +48,10 @@ async_move = wrap(shutil.move)
 #
 #         setattr(cls, wrapper.__name__, wrapper)
 
+# Resources
+# https://phil.tech/2016/http-rest-api-file-uploads/
+# https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Upload_files_to_a_web_server
+
 
 class BaseStorageProcessor(abc.ABC, mode.Service):
 
@@ -60,26 +69,26 @@ class BaseStorageProcessor(abc.ABC, mode.Service):
         payload = self._get_payload(filename, destination, status, reason)
         await self.notifier.acquire(payload)
 
-    def _get_payload(self, filename, destination, status, reason=None):
-        def get_filesize():
+    async def _get_payload(self, filename, destination, status, reason=None):
+        async def get_filesize():
 
-            stat_object = os.stat(filename)
+            stat_object = await aiofiles.os.stat(filename)
             suffix = ["KB", "MB", "GB", "TB"]
             quot = stat_object.st_size
             size = None
             idx = -1
 
-            while quot:
-                size, quot = quot, quot // 1024
+            while math.floor(quot):
+                size, quot = quot, quot / 1024
                 idx += 1
                 # stop on terabytes
                 if idx == len(suffix) - 1:
                     break
 
             if size:
-                size = f"{size} {suffix[idx]}"
+                size = f"{size: .2f} {suffix[idx]}"
             else:
-                size = f"{stat_object.st_size // 1024} {suffix[0]}"
+                size = f"{stat_object.st_size // 1024: .2f} {suffix[0]}"
 
             return size
 
@@ -90,10 +99,10 @@ class BaseStorageProcessor(abc.ABC, mode.Service):
             filename=basename,
             destination=destination,
             extension=extension,
-            processor=self.fancy_name,
+            processor=self.fancy_name.upper(),
             protocol=ProtocolEnum[get_protocol(destination)],
             status=status,
-            size=get_filesize(),
+            size=await get_filesize(),
             reason=reason,
         ).dict()
 
@@ -114,10 +123,10 @@ class LocalStorageProcessor(BaseStorageProcessor):
     fancy_name = "Local processor"
 
     @mode.Service.task
-    async def _process(self, filename: PATH, destination: PATH, **kwargs):
+    async def _process(self, **kwargs):
         await self._move()
 
-    async def _move(self):
+    async def _move(self, **kwargs):
         filename, destination = await self.unprocessed.get()
         try:
             basename = os.path.basename(filename)
@@ -127,18 +136,8 @@ class LocalStorageProcessor(BaseStorageProcessor):
 
         except OSError as exp:
             logger.exception(exp)
-            await self._notify(
-                filename, destination, StatusEnum.FAILED, reason=" ".join(exp.args)
-            )
-
-
-class FtpStorageProcessor(BaseStorageProcessor):
-
-    fancy_name = "FTP processor"
-
-    @mode.Service.task
-    async def _process(self, **kwargs):
-        pass
+            reason = " ".join(exp.args)
+            await self._notify(filename, destination, StatusEnum.FAILED, reason)
 
 
 class HttpStorageProcessor(BaseStorageProcessor):
@@ -147,16 +146,73 @@ class HttpStorageProcessor(BaseStorageProcessor):
 
     @mode.Service.task
     async def _process(self, **kwargs):
-        pass
+        await self._send(**kwargs)
 
-    async def _send(self, filename, destination, **kwargs):
-        # https://docs.aiohttp.org/en/stable/
+    async def _send(self, **kwargs):
         # https://docs.aiohttp.org/en/stable/multipart.html#sending-multipart-requests
-        # https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#multipartform-data
-        async with aiohttp.ClientSession() as session:
-            async with session.post(destination) as response:
-                print("Status:", response.status)
-                print("Content-type:", response.headers["content-type"])
 
-                html = await response.text()
-                print("Body:", html[:15], "...")
+        filename, destination = await self.unprocessed.get()
+
+        retry_options = ExponentialRetry(attempts=3)
+        retry_client = RetryClient(raise_for_status=False, retry_options=retry_options)
+
+        async with aiohttp.MultipartWriter() as writer:
+            try:
+                # FIXME: The content getter must depend on the file size,
+                #  we  must use _send_chunk for big files (State the definition of big)
+                writer.append(await aiofiles.open(filename, "rb"))
+            except OSError as exp:
+                logger.exception(exp)
+                reason = " ".join(exp.args)
+                await self._notify(filename, destination, StatusEnum.FAILED, reason)
+                return
+
+            async with retry_client.post(destination, data=writer) as response:
+                if response.ok:
+                    await self._notify(filename, destination, StatusEnum.SUCCEEDED)
+                else:
+                    reason = await response.text()
+                    reason = f"{response.status} {response.reason}\n\n{reason}"
+                    await self._notify(filename, destination, StatusEnum.FAILED, reason)
+
+    # https://docs.aiohttp.org/en/stable/client_quickstart.html#streaming-uploads
+    async def _send_chunk(self, filename):  # useful for big files
+        async with aiofiles.open(filename, "rb") as f:
+            chunk = await f.read(64 * 1024)
+            while chunk:
+                yield chunk
+                chunk = await f.read(64 * 1024)
+
+
+class FtpStorageProcessor(BaseStorageProcessor):
+
+    fancy_name = "FTP processor"
+
+    @mode.Service.task
+    async def _process(self, **kwargs):
+        await self._send(**kwargs)
+
+    async def _send(self, **kwargs):
+        filename, destination = await self.unprocessed.get()
+
+        try:
+            scheme = parse_obj_as(FtpUrl, destination)
+        except error_wrappers.ValidationError as exp:
+            logger.exception(exp)
+            reason = exp.json()
+            await self._notify(filename, destination, StatusEnum.FAILED, reason)
+            return
+        else:
+            if not all([scheme.host, scheme.port, scheme.user, scheme.password]):
+                reason = f"Missing connection information (host, port, username, password) in {destination}"
+                await self._notify(filename, destination, StatusEnum.FAILED, reason)
+                return
+
+        async with aioftp.Client.context(
+            scheme.host, port=scheme.path, user=scheme.user, password=scheme.password
+        ) as client:
+            try:
+                await client.upload(filename)
+            except aioftp.StatusCodeError as exp:
+                reason = f"Received {exp.received_codes}\n\nExpected{exp.expected_codes}\n\n{exp.info}"
+                await self._notify(filename, destination, StatusEnum.FAILED, reason)
