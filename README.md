9beabc8e70ec91ab579fad3609de70799b35e7cf
Aristide <aristide.bamazi@imsp-uac.org>
2022-09-18 19:33:50 +0100

    add cli and add demonize capabilites

--- README.md
+++ README.md
@@ -8,25 +8,53 @@ to some configured destination.
 
 ## Usage
 
+### Monitor running services [source](https://pypi.org/project/aiomonitor-ng/)
+connect to aiomonitor telnet server by using `nc` utility or other telnet client.
+```shell
+nc 127.0.0.1 50101
+```
+
+### filedispatch cli
+```shell
+usage: filedispatch [-h] [-v] [-H IP] [-P PORT] [-d] [-x] [--no-web-app] [--log-file LOG_FILE] [--log-level LOG_LEVEL] [-p PIDFILE] -c CONFIG
+
+options:
+  -h, --help            show this help message and exit
+  -v, --version         Display the version and exit
+  -H IP, --host IP      web app host ip (default: 127.0.0.1)
+  -P PORT, --port PORT  web app port (default: 3001)
+  -d, --daemon          Run as daemon
+  -x, --exit            Sends SIGTERM to the running daemon
+  --no-web-app          Whether to launch web app
+  --log-file LOG_FILE   Where logs have to come if working in daemon. Ignored if working in foreground.
+  --log-level LOG_LEVEL
+                        Logging level (default: INFO)
+  -p PIDFILE, --pidfile PIDFILE
+                        PID storage path
+  -c CONFIG, --config CONFIG
+                        configuration file to use
+```
+
 ## Features
 
 + Support Local file moving
 + Support Http chunck binary stream file upload
 + Support Ftp file upload 
-+ Provide a monitoring Api
++ Expose a monitoring web api
 + Well tested project
 + ...
 
 ## Main dependencies
 
-+ [pydantic](https://pydantic-docs.helpmanual.io/)
-+ [mode](https://github.com/ask/mode)
-+ [watchfiles](https://watchfiles.helpmanual.io/)
++ [pydantic](https://pypi.org/project/pydantic/)
++ [mode](https://pypi.org/project/mode-ng/)
++ [watchfiles](https://pypi.org/project/watchfiles/)
 + [aiohttp](https://docs.aiohttp.org/en/stable/)
-+ [aioftp](https://github.com/aio-libs/aioftp)
++ [aioftp](https://pypi.org/project/aioftp/)
 + [aiofiles](https://pypi.org/project/aiofiles/)
-+ [pipyka](https://github.com/kayak/pypika)
-+ [aiosqlite](https://aiosqlite.omnilib.dev/en/stable/)
++ [pipyka](https://pypi.org/project/PyPika/)
++ [aiosqlite](https://pypi.org/project/aiosqlite3/)
++ [daemons](https://pypi.org/project/daemons/)
 
 ## Tests
 
--- src/cli.py
+++ src/cli.py
@@ -1,52 +1,152 @@
 import argparse
+import functools
 import os.path
 import logging
+import pathlib
 import sys
+from datetime import datetime
 
+from daemons import daemonizer
+
+from . import __version__
 from .config import Config
 from .watchers import FileWatcher
+from .utils import BASE_DIR, isfile, has_permission
 
 logger = logging.getLogger(__name__)
 
 
-def main():
-    args = add_argumenent()
-    config = args.config.name
-    args.config.close()
-
-    if not check_file_format(config):
-        logger.error(
-            "The config file has wrong extension. Did you provide (.yaml/.yml) file ?"
-        )
-        return
-
-    config = Config(config)()
-
-    if not config:
-        logger.error("The config file is malformed")
-        return
-
-    dispatcher = FileWatcher(config)
-    dispatcher.run()
-
-
-# source: https://stackoverflow.com/a/4042861/13837279
-class CustomArgumentParser(argparse.ArgumentParser):
-    def error(self, message):
-        self.print_usage()
-        sys.exit(2)
-
-
-def add_argumenent():
-    parser = CustomArgumentParser(description="Dispatch new added file in a folder")
-    parser.add_argument("-l", "--log-level", help="logging level")
-    parser.add_argument("config", help="config file path", type=argparse.FileType())
-    args = parser.parse_args()
-    return args
-
-
-def check_file_format(filename, format=None):
-    format = format or ["yml", "yaml"]
+def validate_config(filename):
+    allowed_ext = ["yml", "yaml"]
     _, ext = os.path.splitext(filename)
     ext = ext.removeprefix(".")
-    return ext in format
+
+    if ext not in allowed_ext:
+        raise argparse.ArgumentTypeError(
+            "The config file has wrong extension. Did you provide (.yaml/.yml) file ?"
+        )
+    if not isfile(filename) or not has_permission(filename, mode=os.R_OK):
+        raise argparse.ArgumentError(
+            f"The config file [{filename}] doesn't exists or is not readable."
+        )
+    return pathlib.Path(filename)
+
+
+validate_config.__name__ = "ConfigFile"
+
+
+def validate_log_file(filename):
+    if filename and (
+        not isfile(filename) or not has_permission(filename, mode=os.R_OK)
+    ):
+        raise argparse.ArgumentError(
+            f"The config file [{filename}] doesn't exists or is not readable."
+        )
+    return filename
+
+
+validate_log_file.__name__ = "LogFile"
+
+
+# Inspired by https://github.com/msztolcman/sendria/blob/master/sendria/cli.py
+
+
+def parse_argv(argv: list) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(prog="filedispatch")
+    version = f"%(prog)s {__version__} (github link to filedispatch)"
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=version,
+        help="Display the version and exit",
+    )
+    parser.add_argument(
+        "-H", "--host", metavar="IP", help="web app host ip (default: 127.0.0.1)"
+    )
+    parser.add_argument(
+        "-P", "--port", type=int, metavar="PORT", help="web app port (default: 3001)"
+    )
+    parser.add_argument(
+        "-d", "--daemon", help="Run as daemon", action="store_true", default=False
+    )
+    parser.add_argument(
+        "-x", "--exit", action="store_true", help="Sends SIGTERM to the running daemon"
+    )
+    parser.add_argument(
+        "--no-web-app",
+        help="Whether to launch web app",
+        action="store_true",
+        default=None,
+    )
+    parser.add_argument(
+        "--log-file",
+        type=validate_log_file,
+        help="Where logs have to come if working in daemon. Ignored if working in foreground.",
+    )
+    parser.add_argument(
+        "--log-level",
+        help="Logging level (default: INFO)",
+    )
+
+    parser.add_argument("-p", "--pidfile", help="PID storage path")
+
+    parser.add_argument(
+        "-c",
+        "--config",
+        metavar="CONFIG",
+        type=validate_config,
+        required=True,
+        help="configuration file to use",
+    )
+    return parser.parse_args(argv), parser
+
+
+def main():
+    args, parser = parse_argv(sys.argv[1:])
+    config = Config(args.config)()
+    pidfile = None
+
+    if not config:
+        logger.error(f"The configuration file [{args.config}] is incorrectly formatted")
+        return
+
+    if args.daemon and not args.log_file:
+        args.log_file = pathlib.Path.cwd() / "filedispatch.log"
+
+    if args.daemon or args.exit:
+        if not args.pidfile:
+            parser.error(
+                "Can't run/exit the daemon. Have you forgotten to provide a pidfile ?"
+            )
+
+        pidfile = pathlib.Path(args.pidfile)
+        if not pidfile.is_absolute():
+            pidfile = pidfile.cwd() / pidfile
+
+        if pidfile.exists() and not has_permission(pidfile, mode=os.W_OK):
+            parser.error(f"The pidfile [{pidfile}] is not writable.")
+
+        if not pidfile.exists() and not has_permission(pidfile.parent, mode=os.W_OK):
+            parser.error(f"The pidfile [{pidfile}] is not writable.")
+
+        if args.daemon and args.exit:
+            parser.error("You can't provide --daemon and --exit")
+
+    dispatcher = FileWatcher(
+        config,
+        host=args.host,
+        port=args.port,
+        log_file=args.log_file,
+        log_level=args.log_level,
+        no_web_app=args.no_web_app,
+    )
+
+    if args.daemon or args.exit:
+        demonic = daemonizer.run(pidfile=pidfile)(dispatcher.run)
+        if args.exit:
+            demonic.stop()
+            sys.exit(0)
+        demonic()
+    else:
+        dispatcher.run()
